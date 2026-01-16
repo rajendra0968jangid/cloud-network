c06868b6b1827bd5a9aaaae240d4e62305a612b9
Rajendra jangid <120442903+rajendra0968jangid@users.noreply.github.com>
2026-01-16 10:37:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const ipAddresses = [
   "3.85.205.80",
 ];
 
-const v = "14:24:00";
-const startTime = new Date(`2026-01-15T${v}`).getTime();
+const v = "10:37:00";
+const startTime = new Date(`2026-01-16T${v}`).getTime();
 const counter = "countdown1" // countdown | countdown1
 ////////////aws cloud
 //////////////////////
